2759224825c4752602b1c39d0f1d1518eea513c2
nytheratheone <adindafitriaramadhanii@gmail.com>
2026-04-30 00:46:00 -0400

    Task 2: Add admin classes with QuestionInline, ChoiceInline, QuestionAdmin, LessonAdmin

--- final_project/onlinecourse/admin.py
+++ final_project/onlinecourse/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# Register your models here.
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class LessonsInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonsInline]
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
